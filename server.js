951dafe1d273ea8b80a0df156e32a303b7b34ac9
Ryan Jones <ryandjones14@gmail.com>
2021-10-07 18:45:52 -0500

    adds routes for getting all beer, beer by id, and beer by name

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+const cors = require("cors");
+const dbo = require("./dbConn");
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes"));
+
+// Global error handling
+app.use(function (err, _req, res) {
+  console.error(err.stack);
+  res.json('Something broke!');
+});
+
+// perform a database connection when the server starts
+dbo.connectToServer(function (err) {
+  if (err) {
+    console.error(err);
+    process.exit();
+  }
+
+  // start the Express server
+  app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+  });
+});
